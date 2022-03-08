2d9574705546770acc257e04ec7e916774e57d24
bindi-v <bindi.vaghela@gmail.com>
2022-03-07 20:57:31 -0500

    created Footer.js

--- get-your-goal-react-app/src/components/Footer.js
+++ get-your-goal-react-app/src/components/Footer.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { FaCopyright } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <div>Footer</div>
+     
+    <div className='footer text-center mb-5 '>
+    Created By Group 5 <FaCopyright style={{ color: "#fff"}} /> 2022 All Right Reserved
+    </div>
   )
 }
 
--- get-your-goal-react-app/src/components/Navbar.js
+++ get-your-goal-react-app/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaBars } from 'react-icons';
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
