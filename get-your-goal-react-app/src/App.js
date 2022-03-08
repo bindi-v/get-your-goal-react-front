0ebee58ade8e2e2eb93a9402c8126f4114b276f7
bindi-v <bindi.vaghela@gmail.com>
2022-03-07 20:51:57 -0500

    added Routes in App.js and created Navbar.js with other basic components

--- get-your-goal-react-app/src/App.js
+++ get-your-goal-react-app/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+       <Route path="/" element={<Home/>} />
+       <Route path= "/dashboard" element={<Dashboard/>} />
+       <Route path="/login" element={<Login/>} />
+       <Route path="/signup" element={<SignUp/>} />
+       </Routes>
+       <Footer/>
+    </Router>
+    </>
   );
 }
 
---
+++ get-your-goal-react-app/src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaBars } from 'react-icons';
+
+const Navbar = () => {
+  return (
+      <>
+    <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg navbar-primary bg-light" expand='md'>
+    <div className='container'> 
+    <h2 className="navbar-brand bg-light navbar-primary logo">GetYourGoal</h2> 
+    <button className='navbar-toggler' type="button" data-bs-toggle="collapse" data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+    <FaBars style={{ color: "#rgb(235, 195, 64)"}}/>
+    </button>
+    <div >
+            <ul className="d-flex justify-content-end align-items-end m-0">
+                <li className='nav-link px-2 pb-0'>
+                    <NavLink to='/' className='nav-link' >
+                        Home
+                        </NavLink>
+                </li>
+                <li className='nav-link px-2 pb-0'>
+                 <NavLink to='/dashboard' className="nav-link" >
+                     Dashboard
+                     </NavLink>
+                </li >
+                <li className='nav-link px-2 pb-0'>
+                 <NavLink to='/login' className="nav-link" >
+                     Login
+                     </NavLink>
+                </li>
+                <li className='nav-link px-2 pb-0'>
+                 <NavLink to='/signup' className="nav-link" >
+                     SignUp
+                     </NavLink>
+                </li>
+                </ul>
+            </div> 
+     </div>  
+    </nav>
+    </>
+  )
+}
+
+export default Navbar;
