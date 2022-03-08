dddc0c51714b2d4766605e8f4129d1a39736723f
bindi-v <bindi.vaghela@gmail.com>
2022-03-07 21:02:56 -0500

    created SignUp.js component

--- get-your-goal-react-app/src/components/SignUp.js
+++ get-your-goal-react-app/src/components/SignUp.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Form, Row, Col, Button } from 'react-bootstrap';
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <>
+    <Form  className='text-center'>
+    <Form.Group as={Row} className="mb-3" controlId="formPlaintextFirstName">
+    <Form.Label column sm="2">
+      FirstName
+    </Form.Label>
+    <Col sm="4">
+      <Form.Control type="firstName" placeholder="FirstName" required/>
+    </Col>
+  </Form.Group>
+  <Form.Group as={Row} className="mb-3" controlId="formPlaintextLastName">
+    <Form.Label column sm="2">
+      LastName
+    </Form.Label>
+    <Col sm="4">
+      <Form.Control type="lastName" placeholder="LastName" required />
+    </Col>
+  </Form.Group>
+  <Form.Group as={Row} className="mb-3" controlId="formPlaintextUserName">
+    <Form.Label column sm="2">
+      UserName
+    </Form.Label>
+    <Col sm="4">
+      <Form.Control type="userName" placeholder="UserName" required />
+    </Col>
+  </Form.Group>
+  <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+    <Form.Label column sm="2">
+      Email
+    </Form.Label>
+    <Col sm="4">
+      <Form.Control type="email"  defaultValue="email@example.com" required />
+    </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+    <Form.Label column sm="2">
+      Password
+    </Form.Label>
+    <Col sm="4">
+      <Form.Control type="password" placeholder="Password" required />
+    </Col>
+  </Form.Group>
+  <Button type="submit">Sign Up!</Button>
+</Form>
+</>
   )
 }
 
