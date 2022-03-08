115552772053d03b2d1829ccc515e709846b9f21
bindi-v <bindi.vaghela@gmail.com>
2022-03-07 23:06:03 -0500

    created CommentCard.js that handle delete for Commemt

---
+++ get-your-goal-react-app/src/components/CommentCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function CommentCard({ comment, subNotify }) {
+
+    function handleDelete() {
+        fetch(`http://localhost:8080/comments/${comment.commentId}`, { method: "DELETE" })
+        .then(() => subNotify({ action: "delete-comment", comment: comment}))
+        .catch(error => subNotify({ action: "delete-comment", error: error }));
+    }
+
+  return (
+    <div className='col'>
+        <div className='card'>
+            <div className='card-body'>
+                <h5>Comment: {comment.comment} </h5>
+                <div className='card-footer' d-flex={comment.toString()} justify-content-center={comment.toString()}>
+                   <button className='btn btn-danger mr-3' type="button" onClick={handleDelete}>Delete</button>
+                   <button className='btn btn-secondary' type="button" onClick={() => subNotify({ action: "edit-comment-form", comment: comment})}>Edit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default CommentCard;
